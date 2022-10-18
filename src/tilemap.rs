2082c48e4564c700718642b3e05b65b314ca2c66
Banyen Phung <baphung1740@gmail.com>
2022-10-18 08:01:09 -0700

    map border spawning

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -54,13 +54,13 @@ fn generate_map(mut commands: Commands, ascii: Res<AsciiSheet>) {
     for entry in glob_with("map_blocks/*m*", options).unwrap() {
         match entry {
             Ok(path) => map_block_ids.push(path.display().to_string()),
-            Err(e) => println!("{}", e)
+            Err(e) => println!("{}", e),
         }
     }
 
     // doing this in columns not rows
     for x in -map_size..map_size + 1 {
-        for y in -map_size..map_size {
+        for y in -map_size..map_size + 1 {
             let map_block = MapBlock {
                 x: x - map_size / 2,
                 y: y - map_size / 2,
@@ -77,15 +77,19 @@ fn generate_map(mut commands: Commands, ascii: Res<AsciiSheet>) {
     draw_map_blocks(commands, ascii, map_blocks, map_size);
 }
 
-fn draw_map_blocks(mut commands: Commands, ascii: Res<AsciiSheet>, map_blocks: Vec<MapBlock>, map_size: i32) {
+fn draw_map_blocks(
+    mut commands: Commands,
+    ascii: Res<AsciiSheet>,
+    map_blocks: Vec<MapBlock>,
+    map_size: i32,
+) {
     let mut tiles = Vec::new();
 
-    let mut enemies = Vec::new();
+    let mut enemies: Vec<Entity> = Vec::new();
 
     let total_map_size = ((((map_size as f32) * 2.0) + 1.0) * MAP_BLOCK_X) * TILE_SIZE;
 
     let half_block_size = (32 / 2) * TILE_SIZE as i32;
-    println!("{}", half_block_size);
 
     for map_block in map_blocks {
         let map_file = File::open(map_block.block_id).expect("Map file not found");
@@ -98,12 +102,14 @@ fn draw_map_blocks(mut commands: Commands, ascii: Res<AsciiSheet>, map_blocks: V
 
                     if char == '.' {
                     } else if char == '7' {
-                        let enemy_7 = commands.spawn().id();
-                        enemies.push(enemy_7);
+                        // let enemy_7 = commands.spawn().id();
+                        // enemies.push(enemy_7);
                     } else {
                         let tile_translation = Vec3::new(
-                            (MAP_BLOCK_X * map_block.x as f32 + x as f32 * TILE_SIZE) + half_block_size as f32,
-                            (MAP_BLOCK_Y * map_block.y as f32 + (y as f32) * TILE_SIZE) + half_block_size as f32,
+                            (MAP_BLOCK_X * map_block.x as f32 + x as f32 * TILE_SIZE)
+                                + half_block_size as f32,
+                            (MAP_BLOCK_Y * map_block.y as f32 + (y as f32) * TILE_SIZE)
+                                + half_block_size as f32,
                             1.0,
                         );
 
@@ -121,28 +127,31 @@ fn draw_map_blocks(mut commands: Commands, ascii: Res<AsciiSheet>, map_blocks: V
                 }
             }
         }
+    }
+    let map_border_size: f32 = (((map_size as f32) * 2.0) + 1.0) * MAP_BLOCK_X;
+    let map_border_size_in_tiles: i32 = map_border_size as i32 / TILE_SIZE as i32;
 
-        let map_border_size_in_tiles: i32 = ((((map_size as f32) * 2.0) + 1.0) / TILE_SIZE).round() as i32;
-        let map_border_size_f32: f32 = map_border_size_in_tiles as f32 * TILE_SIZE;
+    for i in 0..map_border_size_in_tiles {
+        let tile_translation = Vec3::new(
+            (i as f32 * TILE_SIZE) - map_border_size / 2.0,
+            map_border_size / 2.0,
+            0.0,
+        );
+        println!("{}", i);
+        let border_tile = spawn_ascii_sprite(
+            &mut commands,
+            &ascii,
+            220,
+            Color::rgb_u8(255, 255, 255),
+            tile_translation,
+            TILE_SIZE,
+        );
 
-        for i in 0..map_border_size_in_tiles {
-            println!("spawning border tile");
-            let tile_translation = Vec3::new((i as f32 * TILE_SIZE) - map_border_size_f32 / 2.0, map_border_size_f32 / 2.0, 0.0);
-            let border_tile = spawn_ascii_sprite(
-                &mut commands,
-                &ascii,
-                4,
-                Color::rgb_u8(255, 255, 255),
-                tile_translation,
-                TILE_SIZE,
-                );
+        commands
+            .entity(border_tile)
+            .insert(Name::new("Border Tile"));
 
-            commands.entity(border_tile).insert(Name::new("Border Tile"));
-
-            tiles.push(border_tile);
-        }
-        
-
+        tiles.push(border_tile);
     }
     let mut map = commands.spawn_bundle(SpatialBundle {
         transform: Transform {
